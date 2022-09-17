c0ef8050b07b4c712cc6d3851f45ce195eefbccf
Divyesh Parmar <divyesh.parmar@postman.com@Divyeshs-MacBook-Pro.local>
2022-09-18 02:19:01 +0530

    backend - get files api have been modified to support currenpathid as query - logger updated with timestamp

--- drive-service/routes/index.js
+++ drive-service/routes/index.js
@@ -4,26 +4,36 @@ const { v4: uuidv4 } = require('uuid');
 const db = require('../database');
 
 router.get('/', (req, res) => {
-    console.log('>>>>base app route has been called', req.params);
+    console.log(':: ::', new Date().toISOString(), '>>>>base app route has been called', req.params);
     res.status(200).json({ data: '<><> drive service has been called on base route <><>', error: null });
 })
 
 
 router.get('/files', (req, res) => {
-    console.log('>>>> /files get route has been called', req.body);
-    const sqlSelect = 'SELECT * FROM drivefiles;';
+    console.log(':: ::', new Date().toISOString(), '>>>> /files get route has been called', req.query);
+    if (!req.body || !req.query.currentPathId) {
+        res.status(400).json({
+            error: {
+                message: 'currentPathId field is required to process this query. It seems to be missing.'
+            }
+        })
+        throw new Error('currentPathId field is required to process this query. It seems to be missing.');
+    }
+
+    // const derivedPathId = req.body.currentPathId === 'root' ? null : req.body.currentPathId
+    const sqlSelect = `SELECT * FROM drivefiles WHERE parentId = "${req.query.currentPathId}";`;
     db.query(sqlSelect, (err, result) => {
         if (err) {
-            console.error("<><>sql query failed<><>", err)
+            console.error(':: ::', new Date().toISOString(), "<><>sql query failed<><>", err)
             res.status(500).json({ error: err, data: null });
         }
-        console.log("<><> made this query <><>", sqlSelect, result);
+        console.log(':: ::', new Date().toISOString(), "<><> made this query <><>", sqlSelect, result);
         res.status(200).json({ error: null, data: result });
     });
 });
 
 router.post('/files/create', (req, res) => {
-    console.log('>>>> /files/insert post route has been called', req.body);
+    console.log(':: ::', new Date().toISOString(), '>>>> /files/insert post route has been called', req.body);
     if (!req.body || !req.body.fileName || !req.body.fileType || !req.body.newFileId) {
         res.status(400).json({
             error: {
@@ -40,7 +50,7 @@ router.post('/files/create', (req, res) => {
 
     db.query(sqlInsert, (err, result) => {
         if (err) {
-            console.error("<><>sql query failed<><>", err);
+            console.error(':: ::', new Date().toISOString(), "<><>sql query failed<><>", err);
             res.status(500).json({ error: err, data: null });
         }
         try {
@@ -57,9 +67,9 @@ router.post('/files/create', (req, res) => {
                     }
                 }
             });
-            console.log("<><> this query was successful <><>", sqlInsert);
+            console.log(':: ::', new Date().toISOString(), "<><> this query was successful <><>", sqlInsert);
         } catch (e) {
-            console.error('<><> there was some error while making this query <><>', sqlInsert, e);
+            console.error(':: ::', new Date().toISOString(), '<><> there was some error while making this query <><>', sqlInsert, e);
         }
     })
 });
